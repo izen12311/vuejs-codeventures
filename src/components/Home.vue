e56124bc5e408ef5e9bf34b49d6ef7354de01310
Lestari Kim <getsolaris@naver.com>
2017-05-27 16:42:30 +0900

    main height fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,16 @@
 ﻿<template>
     <div id="home">
         <header id="myCarousel" class="carousel slide">
-            <!-- Indicators -->
+            <!-- Indicators
             <ol class="carousel-indicators">
                 <!-- 임 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                   <li data-target="#myCarousel" data-slide-to="1"></li>
-                 <li data-target="#myCarousel" data-slide-to="2"></li> -->
-            </ol>
+                 <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol> -->
             <!-- Wrapper for Slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <!-- Set the first background image using inline CSS below. -->
+
                     <img src="../../static/img/wallpaper-hd-background-9.jpg" style="fill">
                     <div class="carousel-caption">
                         <a :href="recruitURL1"><p>청소년 개발팀 코드벤처스에서<br/>활동할 인재를 찾고있습니다.</p>
@@ -18,22 +18,9 @@
                             <p style="font-size:20px;margin-top:-20px;"><br>함께 키워나갈 구성원을 모집중에 있습니다.<br>자세한 내용은 여기를 클릭해주세요 !
                             </p></a>
                     </div>
+
                 </div>
-                <div class="item">
-                    <!-- Set the second background image using inline CSS below. -->
-                    <img src="../../static/img/slide1.jpg" style="fill">
-                    <div class="carousel-caption">
-                        <p>1</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <!-- Set the third background image using inline CSS below. -->
-                    <!--<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>-->
-                    <img src="../../static/img/slide1.jpg" style="fill">
-                    <div class="carousel-caption">
-                        <p>1</p>
-                    </div>
-                </div>
+
             </div>
             <!-- Controls -->
             <!-- 임시 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
@@ -44,8 +31,7 @@
                <!--<span class="icon-next"></span> prev-->
             </a>
         </header>
-        <!-- //
-                  preloader </div> -->
+
 
     </div>
 
